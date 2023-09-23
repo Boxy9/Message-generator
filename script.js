facfe3750a5fd3f4059eb89a7cda972c0ccdf53d
David Boxall <davidboxall01@gmail.com>
2023-09-23 16:48:36 +0100

    Finished code - call wouldYouLike()

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 // It let you select a base and a topping eg. the base would be something like "toast" and
 // the topping would be something like "ketchup" and so it would read "would you like ketchup on your toast".
 // some of the combinations were silly and made us laugh....
-const bases = ['toast','crumpet','waffle','pancake','bread','panini','pizza','cake'];
-const topping = ['mud','kethcup','peas','marmite','pesto','fish','meat','mustard','monkey'];
-const baseIdx = Math.floor(Math.random() * bases.length);
-const toppingIdx = Math.floor(Math.random() * topping.length);
+const wouldYouLike = () => {
+    const bases = ['toast', 'crumpet', 'waffle', 'pancake', 'bread', 'panini', 'pizza', 'cake', 'cracker'];
+    const topping = ['mud', 'kethcup', 'peas', 'marmite', 'pesto', 'fish', 'meat', 'mustard', 'monkey'];
+    const baseIdx = Math.floor(Math.random() * bases.length);
+    const toppingIdx = Math.floor(Math.random() * topping.length);
+    return `Would you like ${topping[toppingIdx]} on your ${bases[baseIdx]} ?`;
+}
+console.log(wouldYouLike());
